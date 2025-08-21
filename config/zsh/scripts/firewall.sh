c5892bb44bfae3e2ace28fab780ee39279a0eb76
Liam Lin <zhonglin0107@gmail.com>
2025-08-21 14:29:10 +0800

    chore(firewall): fix libvirtd no network issue

--- config/zsh/scripts/firewall.sh
+++ config/zsh/scripts/firewall.sh
@@ -34,16 +34,18 @@ iptables -A INPUT -p icmp -j ACCEPT
 # libvirt (virbr0) NAT 與轉發
 # -------------------------------
 log "允許 libvirt VM 透過 virbr0 出網"
-iptables -A FORWARD -i virbr0 -j ACCEPT
-iptables -A FORWARD -o virbr0 -j ACCEPT
-iptables -t nat -A POSTROUTING -s 192.168.122.0/24 ! -o virbr0 -j MASQUERADE
+# VM -> WWW
+iptables -A FORWARD -i virbr0 -o wlo1 -j ACCEPT
+# WWW -> VM
+iptables -A FORWARD -i wlo1 -o virbr0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -A POSTROUTING -s 192.168.122.0/24 -o wlo1 -j MASQUERADE
 
 # -------------------------------
 # docker (docker0) NAT 與轉發
 # -------------------------------
 log "允許 Docker 容器透過 docker0 出網"
-iptables -A FORWARD -i docker0 -j ACCEPT
-iptables -A FORWARD -o docker0 -j ACCEPT
+iptables -A FORWARD -i docker0 -o wlo1 -j ACCEPT
+iptables -A FORWARD -i wlo1 -o docker0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -t nat -A POSTROUTING -s 172.17.0.0/16 ! -o docker0 -j MASQUERADE
 
 log "拒絕其他封包前 log 一下（方便 debug）"
