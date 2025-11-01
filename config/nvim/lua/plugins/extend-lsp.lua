6f0884766763b87ffd70f2bd98816fa8c981191b
Liam Lin <zhonglin0107@gmail.com>
2025-11-01 10:39:04 +0800

    chore(nvim): disable virtual linter text

--- config/nvim/lua/plugins/extend-lsp.lua
+++ config/nvim/lua/plugins/extend-lsp.lua
@@ -6,11 +6,12 @@ return {
       diagnostics = {
         underline = true,
         update_in_insert = false,
-        virtual_text = {
-          spacing = 4,
-          source = "if_many",
-          prefix = "●",
-        },
+        -- virtual_text = {
+        --   spacing = 4,
+        --   source = "if_many",
+        --   prefix = "●",
+        -- },
+        virtual_text = false,
         severity_sort = true,
         float = { border = "single" },
         signs = {
