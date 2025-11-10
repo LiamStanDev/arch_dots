adac28c11f19f2c2ceb5a9c0d24e0ab10b3c5e3e
Liam Lin <zhonglin0107@gmail.com>
2025-11-11 06:54:25 +0800

    chore(nvim): use yazi in dashboard

--- config/nvim/lua/plugins/extend-dashboard.lua
+++ config/nvim/lua/plugins/extend-dashboard.lua
@@ -26,7 +26,8 @@ return {
         -- stylua: ignore
         ---@type snacks.dashboard.Item[]
         keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "f", desc = "Find File", action = ":Yazi" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
